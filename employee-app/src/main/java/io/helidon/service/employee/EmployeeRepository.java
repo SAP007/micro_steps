db13b90c6d8fde657f64c3896520f9858c5c5a60
Sofie-Amalie <s174255@student.dtu>
2022-09-29 16:06:13 +0200

    DAO step completed

---
+++ employee-app/src/main/java/io/helidon/service/employee/EmployeeRepository.java
@@ -0,0 +1,30 @@
+package io.helidon.service.employee;
+
+import io.helidon.config.Config;
+
+import java.util.List;
+
+public interface EmployeeRepository {
+
+    public static EmployeeRepository create(String driverType, Config config) {
+        switch (driverType) {
+            case "Array":
+                return new EmployeeRepositoryImpl();
+        /*case "Oracle":
+            return new EmployeeRepositoryImplDB(config);*/
+            default:
+                // Array is default
+                return new EmployeeRepositoryImpl();
+        }
+    }
+
+    public List<Employee> getAll();
+    public List<Employee> getByLastName(String lastName);
+    public List<Employee> getByTitle(String title);
+    public List<Employee> getByDepartment(String department);
+    public Employee save(Employee employee); // Add new employee
+    public Employee update(Employee updatedEmployee, String id);
+    public void deleteById(String id);
+    public Employee getById(String id);
+    public boolean isIdFound(String id);
+}
